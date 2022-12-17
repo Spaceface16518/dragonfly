ab71061987099644c5007d34a9897d13be5af065
Spaceface16518 <amrit.rathie@gmail.com>
2022-12-17 01:09:38 -0600

    format with prettier

--- src/drop.ts
+++ src/drop.ts
@@ -1,16 +1,15 @@
 export default function setupDrop($drop: HTMLDivElement) {
-    // prevent browser's default drag-and-drop action in the drop zone
-    const preventDefault = (e: Event) => e.preventDefault()
-    $drop.ondragover = preventDefault;
-    $drop.ondragenter = preventDefault;
-    // handle drag-and-drop of files
-    $drop.ondrop = e => {
-        preventDefault(e);
-        // set the file input's value to the dropped files
-        $file.files = e.dataTransfer?.files || null;
-    }
+  // prevent browser's default drag-and-drop action in the drop zone
+  const preventDefault = (e: Event) => e.preventDefault();
+  $drop.ondragover = preventDefault;
+  $drop.ondragenter = preventDefault;
+  // handle drag-and-drop of files
+  $drop.ondrop = (e) => {
+    preventDefault(e);
+    // set the file input's value to the dropped files
+    $file.files = e.dataTransfer?.files || null;
+  };
 
-    const $file = $drop.querySelector<HTMLInputElement>('#file')!
-    $drop.onclick = () => $file.click();
+  const $file = $drop.querySelector<HTMLInputElement>("#file")!;
+  $drop.onclick = () => $file.click();
 }
-
--- src/toast.ts
+++ src/toast.ts
@@ -1,14 +1,15 @@
 export default function setupToast($toastContainer: HTMLDivElement) {
-	const $toastTemplate = $toastContainer.querySelector<HTMLTemplateElement>('#toast-template')!.content;
+  const $toastTemplate =
+    $toastContainer.querySelector<HTMLTemplateElement>(
+      "#toast-template"
+    )!.content;
 
-	return function showToast(message: string, timeout: number = 5000) {
-		// create a new toast element
-		const $toast = $toastTemplate.cloneNode(true) as HTMLDivElement;
-		// set the toast contents
-		$toast.innerText = message;
-		// hide the message after timeout
-		setTimeout(() => $toastContainer.removeChild($toast), timeout);
-	}
-
+  return function showToast(message: string, timeout: number = 5000) {
+    // create a new toast element
+    const $toast = $toastTemplate.cloneNode(true) as HTMLDivElement;
+    // set the toast contents
+    $toast.innerText = message;
+    // hide the message after timeout
+    setTimeout(() => $toastContainer.removeChild($toast), timeout);
+  };
 }
-
